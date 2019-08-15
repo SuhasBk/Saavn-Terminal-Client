e90e77b3f00a6fd4fab8f2d7032bdca406d0622b
Suhas B Kowligi <kowligi1998@gmail.com>
2019-08-15 12:57:46 +0530

    script updates

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 chmod 777 -R .;
 
-echo '*This installation script is for Linux users only.* Use this script to automatically set-up all dependencies required to run saavn.py';
+echo -e '*This installation script is for Linux users only.*\nUse this script to automatically set-up all dependencies required to run saavn.py';
 
 sleep 5;
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz";
 tar -xvf gecko*;
 rm gecko*.tar.gz
 
-echo 'The file has been successfully downloaded. Please enter the password to put it in the path...';
+echo -e '\nThe file has been successfully downloaded. Please enter the password to put it in the path...';
 sleep 2;
 
 sudo mv gecko* /usr/local/bin/;
 
-echo 'You are all set! Enjoy Saavn...'
+echo -e '\nCool! Everything is ready now... Do you want to automatically install requirements using *pip* as well?\n'
+read ans
+if [ $ans == "y" ]||[ $ans == 'Y' ];
+then
+    python3 -m pip install --user -r requirements.txt;
+else
+    echo -e '\nYou are all set! Enjoy Saavn...';
